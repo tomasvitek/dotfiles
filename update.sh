ec1681a3d0eb08bc0425c4a45f762555d8d7dc3d
Tomáš Vítek <me@tomasvitek.com>
2016-12-11 17:22:43 +0000

    Fix update script

--- update.sh
+++ update.sh
@@ -3,7 +3,25 @@
 cd "$(dirname "${BASH_SOURCE}")";
 
 function doUpdate() {
-	rsync -avh --no-perms --existing . ~;
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude "bootstrap.sh" \
+		--exclude "update.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE.md" \
+		--exclude "Applications" \
+		--exclude "Desktop" \
+		--exclude "Documents" \
+		--exclude "Downloads" \
+		--exclude "Dropbox" \
+		--exclude "Library" \
+		--exclude "Movies" \
+		--exclude "Music" \
+		--exclude "Pictures" \
+		--exclude "Public" \
+		--exclude "Trash" \
+		--exclude "Work" \
+		-avh --no-perms --existing . ~;
 	source ~/.bash_profile;
 }
 
