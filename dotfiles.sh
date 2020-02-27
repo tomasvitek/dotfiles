aea6082932d325d291df9de348f277b988a59613
Tomáš Vítek <me@tomasvitek.com>
2020-02-27 21:38:41 +0000

    Don't source unused .bash_profile when bootstraping

--- dotfiles.sh
+++ dotfiles.sh
@@ -36,7 +36,7 @@ function doBootstrap() {
 
 	echo .
 	echo "dotfiles loaded into your home directory."
-	source ~/.bash_profile
+	## no need on server --> source ~/.bash_profile
 }
 
 function doBackup() {
